d3e889f3c442abc6232cf214adf0888cac53c85d
Trystan VALENTIN <trystan.valentin@gmail.com>
2021-03-05 11:20:10 +0100

    fix/ les bananes tombent

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-
-// document.getElementById("apparition").addEventListener("click"),function(){
-//     document.getElementById("banane").innerHTML(<img src="img/Plan de travail 8.png" alt=""></img>);
-// };
-
 function banana(){
  let banane = document.createElement("IMG");
+
+ //document.location.reload(banane);
  banane.setAttribute( "src", "img/Plan de travail 3.png")
  document.body.appendChild(banane);   
 }
