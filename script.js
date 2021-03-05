1613fccc638a7c7ecadd03a033117a95db951de5
Trystan VALENTIN <trystan.valentin@gmail.com>
2021-03-05 10:04:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// document.getElementById("apparition").addEventListener("click"),function(){
+//     document.getElementById("banane").innerHTML(<img src="img/Plan de travail 8.png" alt=""></img>);
+// };
+
+function banana(){
+ let banane = document.createElement("IMG");
+ banane.setAttribute( "src", "img/Plan de travail 3.png")
+ document.body.appendChild(banane);   
+}
