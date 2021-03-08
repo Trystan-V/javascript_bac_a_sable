4be695f1f16e9c0226aaf800cf041b0395d7d1b6
Trystan VALENTIN <trystan.valentin@gmail.com>
2021-03-08 10:05:16 +0100

    fix/ mini-refactor

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function banana(){
  let banane = document.createElement("IMG");
-
- //document.location.reload(banane);
- banane.setAttribute( "src", "img/Plan de travail 3.png")
+ banane.setAttribute( "src", "img/Plan_de_travail_8.png");
  document.body.appendChild(banane); 
 }
 
